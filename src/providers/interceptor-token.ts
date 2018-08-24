3f356cfe57247c84a9d2f72090d0cf8065b3987c
Guilherme Nass <nassguilherme@gmail.com>
2018-08-24 01:05:48 -0300

    adicionei o interceptor para cada requisição http

---
+++ src/providers/interceptor-token.ts
@@ -0,0 +1,37 @@
+import { AlertController } from 'ionic-angular';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage';
+ 
+import { Observable } from 'rxjs';
+import { _throw } from 'rxjs/observable/throw';
+import { mergeMap } from 'rxjs/operator/mergeMap';
+
+
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor {
+ 
+  constructor(private storage: Storage, private alertCtrl: AlertController) { }
+ 
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+    request = request.clone({
+      setHeaders: {
+        Authorization: `Bearer token teste` 
+      }
+    });
+
+    return next.handle(request).do((event: HttpEvent<any>) => {
+        if (event instanceof HttpResponse) {
+            // do stuff with response if you want
+        }
+    },
+    (err: any) => {
+        if (err instanceof HttpErrorResponse) {
+            if (err.status === 401) {
+                // window.location.href = environment.loginUrl;
+            }
+        }
+    });
+  }
+}
